81ee244b68b48fb9e6abc4f57b56477f4630ee8e
agm221b <adityamishr123@gmail.com>
2020-05-02 17:17:23 +0530

    create master and first push

---
+++ src/test/java/com/me/example/KalMeds/KalMedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.example.KalMeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalMedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
